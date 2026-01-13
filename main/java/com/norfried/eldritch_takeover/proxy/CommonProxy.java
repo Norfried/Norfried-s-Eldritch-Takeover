d9aa6cc0a1297b936c160b0d951d24bbcb766975
Norfried Svenson <gorloththevikingwarlord@gmail.com>
2026-01-12 19:19:31 -0600

    Add files via upload
    Serverside fixes.

--- main/java/com/norfried/eldritch_takeover/proxy/CommonProxy.java
+++ main/java/com/norfried/eldritch_takeover/proxy/CommonProxy.java
@@ -5,9 +5,11 @@ import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class CommonProxy
 {
+    @SideOnly(Side.CLIENT)
     public void registerItemRenderer(Item item, int meta, String id) {}
 
     public void preInitRegisteries(FMLPreInitializationEvent event)
